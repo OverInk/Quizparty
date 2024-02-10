c8e05e2c9141305dacf2254e14fad249bb2577f9
OverInk <dostoevskiy201@mail.ru>
2024-02-10 23:58:33 +0300

    new header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,19 +5,19 @@ export default function Header() {
   return (
     <div className={style.header}>
       <div className={style.header_title}>
-        <p>Главная</p>
-        <p>Расписание</p>
-        <p>Прошедшие игры</p>
-        <p>О нас</p>
+        <p className={style.header_chapter}>Главная</p>
+        <p className={style.header_chapter}>Расписание</p>
+        <p className={style.header_chapter}>Прошедшие игры</p>
+        <p className={style.header_chapter}>О нас</p>
       </div>
       <div className={style.header_media}>
         <div>
-          <img src="/img/media_vk.svg" width={30} height={30} alt="Vk" />
+          <img src="/img/media_vk.svg" alt="Vk" />
           <img src="/img/media_whatsapp.svg" alt="WhatsApp" />
         </div>
-        <div>
+        <div className={style.header_button}>
           <img src="/img/location.svg" alt="Location" />
-          <p>Москва</p>
+          <p className={style.header_button_citys}>Москва</p>
         </div>
       </div>
     </div>
