35f4c15892a3387b851e3c399854511539ce1be0
OverInk <dostoevskiy201@mail.ru>
2024-02-14 22:50:25 +0300

    New main

--- src/components/MainInfo/Maininfo.jsx
+++ src/components/MainInfo/Maininfo.jsx
@@ -3,10 +3,17 @@ import style from './Maininfo.module.scss';
 
 export default function Maininfo() {
   return (
-    <div className={style.main_background}>
-      <div>
-        <h1>Over-time квиз в IT </h1>
-        <p>Делаем викторины по вашим любимым вселенным и не только!</p>
+    <div className={style.main_background + ' ' + style.filter}>
+      <div className={style.main_tablegames}>
+        <div className={style.main_table}>
+          <h1>
+            Over-time квиз <br /> в IT{' '}
+          </h1>
+          <p>
+            Делаем викторины по вашим любимым <br /> вселенным и не только!
+          </p>
+        </div>
+        <button>Расписание игр</button>
       </div>
       <div>
         <img src="/img/logo.svg" alt="Logo" />
