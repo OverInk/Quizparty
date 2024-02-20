28a182fc7c1c7b76ea78a129108fb9edbe9bbeaa
OverInk <dostoevskiy201@mail.ru>
2024-02-20 23:55:06 +0300

    New cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import Maininfo from './components/MainInfo/Maininfo';
+import Cards from './components/Cards/Cards';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Maininfo />
+      <Cards />
     </div>
   );
 }
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Cards() {
+  return (
+    <div>
+      <div>
+        <img src="./img/product2.svg" alt="Logo" />
+      </div>
+      <div>
+        <div>
+          <h2></h2>
+          <p></p>
+        </div>
+        <div>
+          <b></b>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
