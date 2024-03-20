a29ecd7bd09b4ed083a8b799f8986fb6e71f53ba
OverInk <dostoevskiy201@mail.ru>
2024-03-20 23:24:37 +0300

    New

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Maininfo from './components/MainInfo/Maininfo';
 import Cards from './components/Cards';
 import AdditionalInfo from './components/AdditionalInfo';
+import Questions from './components/Questions';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Maininfo />
       <Cards />
       <AdditionalInfo />
+      <Questions />
     </div>
   );
 }
