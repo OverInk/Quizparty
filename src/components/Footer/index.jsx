6168d6552fd0ef49722fe227215c87ffde8ebd54
OverInk <dostoevskiy201@mail.ru>
2024-03-28 17:53:07 +0300

    New

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+	 <div>
+		<div>
+			<img/>
+			<div>
+				<ul>
+					<li>Главная</li>
+					<li></li>
+					<li>Прошедшие игры</li>
+					<li>О нас</li>
+				</ul>
+				<div>
+					<img/>
+					<img/>
+				</div>
+				<div>Оставить отзыв</div>
+			</div>
+		</div>
+		<div>Политика конфиденциальности</div>
+	 </div>
+  )
+}
