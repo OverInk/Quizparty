9ed29b043f27261bc573448edf1b2123e40aa3d4
OverInk <dostoevskiy201@mail.ru>
2024-03-31 21:47:22 +0300

    New

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,10 +4,10 @@ import style from './Footer.module.scss';
 export default function Footer() {
   return (
     <div className={style.footer}>
-      <div>
+      <div className={style.footer_flex}>
         <img src="./img/littleLogo.svg" />
-        <div>
-          <ul>
+        <div className={style.footer_boxtitles}>
+          <ul className={style.footer_titles}>
             <li>Главная</li>
             <li></li>
             <li>Прошедшие игры</li>
